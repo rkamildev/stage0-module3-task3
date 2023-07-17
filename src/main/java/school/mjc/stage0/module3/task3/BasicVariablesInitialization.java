967266135fe0698499dc1d0cfbc650c6aacab7bc
rkamildev <rkamil.int@gmail.com>
2023-07-17 20:27:40 +0500

    Var val change

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.printf("%d\n%d\n%d", first, second, third);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, second = 10, third = 100;
+        System.out.printf("%d\n%d\n%d\n", first, second, third);
+        int linkToFirst = first, linkToSecond = 10, linkToThird = 100;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.printf("%d\n%d\n%d\n%d\n%d\n%d", first, second, third, linkToFirst, linkToSecond, linkToThird);
     }
 }
